defc61b919414d1f9b6664b8969790ac1bc39bd8
pass1enator@gmail.com <pass1enator@gmail.com>
2024-03-13 11:41:13 +0100

    inicial

---
+++ app/src/main/java/ies/eed/practicajunit/balls/BallType.java
@@ -0,0 +1,29 @@
+package ies.eed.practicajunit.balls;
+
+public enum BallType {
+    EXTRABIG(48, 40, 1),
+    BIG(32, 26, 52),
+    MEDIUM(16, 14, 86),
+    LITTLE(8, 7, 106);
+    protected final int width;
+    protected final int heigth;
+    protected final int starx;
+
+    BallType(int width, int height, int startx) {
+        this.width = width;
+        this.heigth = height;
+        this.starx = startx;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeigth() {
+        return heigth;
+    }
+
+    public int getStartx() {
+        return starx;
+    }
+}
